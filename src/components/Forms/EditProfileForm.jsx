baeff7163b4adbc9235616ad6070860c1be5a4b2
FernandoGavilan2000 <blackhu3@gmail.com>
2021-03-03 18:53:07 -0500

    EditProfile

--- src/components/Forms/EditProfileForm.jsx
+++ src/components/Forms/EditProfileForm.jsx
@@ -1,152 +1,276 @@
-import React from 'react'
-import { Col, Container, Row, Image, Form, Button } from "react-bootstrap";
-import { useForm } from "react-hook-form";
+import React from 'react';
+import { Col, Container, Row, Image, Form, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 export const EditProfileForm = () => {
-    const methods = useForm({ mode: "onChange" });
+	const methods = useForm({ mode: 'onChange' });
 
-    return (
-        <>
-            <Form className="mb-5">
-                {/* ----------------***FILA 1***--------------- */}
-                <Form.Row>
-                    {/* ---------------USUARIO---------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">Nick:</Form.Label>
-                            <i className="fa fa-user fa-lg fa-fw" aria-hidden="true" />
-                            <Form.Control type="text" />
-                        </Form.Group>
-                    </Col>
-                    {/* -----------------------CORREO------------------------ */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">Correo:</Form.Label>
-                            <i className="fas fa-envelope fa-lg fa-fw" aria-hidden="true" />
-                            <Form.Control type="text" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                 {/* ----------------***FILA 2***----------------- */}
-                <Form.Row>
-                    {/* --------------GENERO--------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">
-                                Genero:
-                            </Form.Label>
-                            <Form.Control as="select" className="mt-2" >
-                                <option>Seleccione un genero</option>
-                                <option>Masculino</option>
-                                <option>Femenino</option>
-                                <option>No Binario</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                    {/* --------------FECHA DE NACIMIENTO--------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                                <Form.Label className="text_label">
-                                    Fecha nacimiento:
-                                </Form.Label>
-                                <Form.Control type="date" /> 
-                        </Form.Group>
-                    </Col>
-                    {/* --------------PAIS--------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                                <Form.Label className="text_label">
-                                    Pais:
-                                </Form.Label>
-                                <Form.Control type="text" /> 
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                {/* -------------***FILA 3***----------------- */}
-                <Form.Row>
-                    {/* --------------AREA DE CONOCIMIENTO--------------- */}
-                    <Col xs="12" >
-                        <Form.Group>
-                            <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">
-                                Area de conocimiento:
-                            </Form.Label>
-                            <Form.Control as="select" className="mt-2" >
-                                <option>Select algo</option>
-                                <option>Front End</option>
-                                <option>Back End</option>
-                                <option>Dev Ops</option>
-                                <option>Video Game</option>
-                                <option>Developers</option>
-                                <option>UI/UX</option>
-                                <option>Database Developer</option>
-                                <option>Cloud Computing</option>
-                            </Form.Control>
-                        </Form.Group>
-                        </Form.Group>
-                    </Col>
-                    {/* ----------------CONTRASEÑA--------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">Contraseña:</Form.Label>
-                            <i className="fas fa-lock fa-lg fa-fw" aria-hidden="true" />
-                            <Form.Control type="password" />
-                        </Form.Group>
-                    </Col>
-                    {/* ----------------CONFIRMAR CONTRASEÑA--------------- */}
-                    <Col>
-                        <Form.Group className="inputWithIcon">
-                            <Form.Label className="text_label">Confirmar Contraseña:</Form.Label>
-                            <i className="fas fa-lock fa-lg fa-fw" aria-hidden="true" />
-                            <Form.Control type="password" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                {/* ----------***FILA 4***----------- */}
-                    <h4 className="text-center text_label">Redes Sociales</h4>
-                <Form.Row>
-                    {/* --------FACEBOOK------ */}
-                    <Col xs="6" >
-                        <Form.Group className="d-flex align-items-center">
-                            <i class="fab fa-facebook-square fa-2x pr-1" style={{color: "#0f8cf0"}} />
-                            <Form.Control />
-                        </Form.Group>
-                    </Col>
-                    {/* --------GITHUB------ */}
-                    <Col xs="6" >
-                        <Form.Group className="d-flex align-items-center">
-                            <i class="fab fa-github fa-2x pr-1" />
-                            <Form.Control />
-                        </Form.Group>
-                    </Col>
-                    {/* --------LINKEDIN------ */}
-                    <Col xs="6" >
-                        <Form.Group className="d-flex align-items-center">
-                            <i class="fab fa-linkedin fa-2x pr-1" style={{color: "#0c65c2"}} />
-                            <Form.Control />
-                        </Form.Group>
-                    </Col>
-                    {/* --------TWITTER------ */}
-                    <Col xs="6" >
-                        <Form.Group className="d-flex align-items-center">
-                            <i class="fab fa-twitter-square fa-2x pr-1" style={{color: "#00a3f2"}} />
-                            <Form.Control />
-                        </Form.Group>
-                    </Col>
-                    {/* --------BIOGRAFIA------ */}
-                    <Col>
-                        <Form.Group>
-                            <Form.Label className="text_label">Biografia:</Form.Label>
-                            <Form.Control as="textarea" rows={3}/>
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                    <div className="w-100 text-right">
-                        <Button type="submit" className="login_button ml-3">
-                            Guardar cambios
-                        </Button>
-                    </div>
-            </Form>
-        </>
-    )
-}
+	return (
+		<>
+			<Form className='mb-5'>
+				{/* ----------------***FILA 1***--------------- */}
+				<Form.Row>
+					{/* ---------------USUARIO---------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>Nick:</Form.Label>
+							<i
+								className='fa fa-user fa-lg fa-fw'
+								aria-hidden='true'
+							/>
+							<Form.Control
+								type='text'
+								name='nickname'
+								ref={methods.register({
+									required:
+										'No puedes dejar este campo vacio',
+								})}
+							/>
+						</Form.Group>
+					</Col>
+					{/* -----------------------CORREO------------------------ */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>
+								Correo:
+							</Form.Label>
+							<i
+								className='fas fa-envelope fa-lg fa-fw'
+								aria-hidden='true'
+							/>
+							<Form.Control
+								type='text'
+								name='email'
+								readOnly
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+				</Form.Row>
+				{methods.errors.nickname && (
+					<li className='text_label text-danger'>
+						{' '}
+						{methods.errors.nickname.message}{' '}
+					</li>
+				)}
+				{/* ----------------***FILA 2***----------------- */}
+				<Form.Row>
+					{/* --------------GENERO--------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>
+								Genero:
+							</Form.Label>
+							<Form.Control
+								as='select'
+								className='mt-2'
+								name='gender'
+								ref={methods.register}
+							>
+								<option>Seleccione un genero</option>
+								<option>Masculino</option>
+								<option>Femenino</option>
+								<option>No Binario</option>
+							</Form.Control>
+						</Form.Group>
+					</Col>
+					{/* --------------FECHA DE NACIMIENTO--------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>
+								Fecha nacimiento:
+							</Form.Label>
+							<Form.Control
+								type='date'
+								name='birthday'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+					{/* --------------PAIS--------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>Pais:</Form.Label>
+							<Form.Control
+								type='text'
+								name='country'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+				</Form.Row>
+				{/* -------------***FILA 3***----------------- */}
+				<Form.Row>
+					{/* --------------AREA DE CONOCIMIENTO--------------- */}
+					<Col xs='12'>
+						<Form.Group>
+							<Form.Group className='inputWithIcon'>
+								<Form.Label className='text_label'>
+									Area de conocimiento:
+								</Form.Label>
+								<Form.Control
+									as='select'
+									className='mt-2'
+									name='field'
+									ref={methods.register}
+								>
+									<option>Select algo</option>
+									<option>Front End</option>
+									<option>Back End</option>
+									<option>Dev Ops</option>
+									<option>Video Game</option>
+									<option>Developers</option>
+									<option>UI/UX</option>
+									<option>
+										Database Developer
+									</option>
+									<option>
+										Cloud Computing
+									</option>
+								</Form.Control>
+							</Form.Group>
+						</Form.Group>
+					</Col>
+					{/* ----------------CONTRASEÑA--------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>
+								Contraseña:
+							</Form.Label>
+							<i
+								className='fas fa-lock fa-lg fa-fw'
+								aria-hidden='true'
+							/>
+							<Form.Control
+								type='password'
+								name='new_password'
+								ref={methods.register({
+									required:
+										'Porfavor complete este campo',
+									pattern: {
+										value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g,
+										message: `La contraseña debe tener al menos 8 caracteres, e incluir un numero `,
+									},
+								})}
+							/>
+						</Form.Group>
+					</Col>
+					{/* ----------------CONFIRMAR CONTRASEÑA--------------- */}
+					<Col>
+						<Form.Group className='inputWithIcon'>
+							<Form.Label className='text_label'>
+								Confirmar Contraseña:
+							</Form.Label>
+							<i
+								className='fas fa-lock fa-lg fa-fw'
+								aria-hidden='true'
+							/>
+							<Form.Control
+								type='password'
+								name='conf_password'
+								ref={methods.register({
+									required:
+										'Porfavor complete este campo',
+									validate: () => {
+										if (
+											methods.getValues(
+												'new_password'
+											) ===
+											methods.getValues(
+												'conf_password'
+											)
+										)
+											return true;
+										else
+											return 'Las contraseñas no concuerdan';
+									},
+								})}
+							/>
+						</Form.Group>
+					</Col>
+				</Form.Row>
+				{methods.errors.conf_password && (
+					<li className='text_label text-danger'>
+						{methods.errors.conf_password.message}
+					</li>
+				)}
+				{/* ----------***FILA 4***----------- */}
+				<h4 className='text-center text_label'>Redes Sociales</h4>
+				<Form.Row>
+					{/* --------FACEBOOK------ */}
+					<Col xs='6'>
+						<Form.Group className='d-flex align-items-center'>
+							<i
+								class='fab fa-facebook-square fa-2x pr-1'
+								style={{ color: '#0f8cf0' }}
+							/>
+							<Form.Control
+								type='text'
+								name='link_facebook'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+					{/* --------GITHUB------ */}
+					<Col xs='6'>
+						<Form.Group className='d-flex align-items-center'>
+							<i class='fab fa-github fa-2x pr-1' />
+							<Form.Control
+								type='text'
+								name='link_github'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+					{/* --------LINKEDIN------ */}
+					<Col xs='6'>
+						<Form.Group className='d-flex align-items-center'>
+							<i
+								class='fab fa-linkedin fa-2x pr-1'
+								style={{ color: '#0c65c2' }}
+							/>
+							<Form.Control
+								type='text'
+								name='link_linkedin'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+					{/* --------TWITTER------ */}
+					<Col xs='6'>
+						<Form.Group className='d-flex align-items-center'>
+							<i
+								class='fab fa-twitter-square fa-2x pr-1'
+								style={{ color: '#00a3f2' }}
+							/>
+							<Form.Control
+								type='text'
+								name='link_twitter'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+					{/* --------BIOGRAFIA------ */}
+					<Col>
+						<Form.Group>
+							<Form.Label className='text_label'>
+								Biografia:
+							</Form.Label>
+							<Form.Control
+								as='textarea'
+								rows={3}
+								name='biography'
+								ref={methods.register}
+							/>
+						</Form.Group>
+					</Col>
+				</Form.Row>
+				<div className='w-100 text-right'>
+					<Button type='submit' className='login_button ml-3'>
+						Guardar cambios
+					</Button>
+				</div>
+			</Form>
+		</>
+	);
+};
--- src/components/Main/TabsProfile/EditProfile.jsx
+++ src/components/Main/TabsProfile/EditProfile.jsx
@@ -1,49 +1,81 @@
-import React from "react";
-import { Col, Container, Row, Image, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { EditProfileForm } from "../../Forms/EditProfileForm";
+import React from 'react';
+import { Col, Container, Row, Image, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { EditProfileForm } from '../../Forms/EditProfileForm';
 
 export const EditProfile = () => {
-  const methods = useForm({ mode: "onChange" });
-  return (
-    <Container fluid style={{minHeight: "100vh"}}>
-      <Row className={"border-bottom py-3"}>
-        <Col lg={4} md={12} className={"text-center"}>
-          <Link to={"/sub/profile"}>
-            <i class="fas fa-arrow-left" style={{ fontSize: "25px" }}></i>
-          </Link>
-        </Col>
-        <Col xl={8} lg={8} md={10} sm={12} xs={12}>
-          <p className={"text-edit"}>Editar perfil</p>
-        </Col>
-      </Row>
-      <Row className={"d-flex justify-content-center"}>
-        <Col xl={12} lg={12} md={12} sm={12} xs={12} className={"my-3"}>
-          <div className={"d-flex justify-content-center"}>
-            <div
-              id={"image-profile"}
-              className={
-                "rounded-circle position-relative pointer overflow-hidden"
-              }
-              style={{ width: "120px" }}
-            >
-              <Image
-                src={
-                  "https://i.pinimg.com/736x/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg"
-                }
-                width={"120"}
-              />
-              <span className={"camera text-center py-1"}>
-                <i class="fas fa-camera-retro"></i>
-              </span>
-            </div>
-          </div>
-        </Col>
-        <Col lg={6} md={12}>
-          <EditProfileForm />
-        </Col>
-      </Row>
-    </Container>
-  );
+	const methods = useForm({ mode: 'onChange' });
+
+	const previewPhoto = () => {
+		const newImage = document.querySelector('#imgupload').files[0];
+		let reader = new FileReader();
+
+		reader.onload = function () {
+			document.querySelector('#imgProfileUser').src = this.result;
+		};
+		if (newImage) {
+			reader.readAsDataURL(newImage);
+		} else {
+			document.querySelector('#imgProfileUser').src = '';
+		}
+	};
+	return (
+		<Container fluid style={{ minHeight: '100vh' }}>
+			<Row className={'border-bottom py-3'}>
+				<Col lg={4} md={12} className={'text-center'}>
+					<Link to={'/sub/profile'}>
+						<i class='fas fa-arrow-left' style={{ fontSize: '25px' }}></i>
+					</Link>
+				</Col>
+				<Col xl={8} lg={8} md={10} sm={12} xs={12}>
+					<p className={'text-edit'}>Editar perfil</p>
+				</Col>
+			</Row>
+			<Row className={'d-flex justify-content-center'}>
+				<Col xl={12} lg={12} md={12} sm={12} xs={12} className={'my-3'}>
+					<div className={'d-flex justify-content-center'}>
+						<div
+							id={'image-profile'}
+							className={
+								'rounded-circle position-relative pointer overflow-hidden'
+							}
+							style={{ width: '120px' }}
+						>
+							<Image
+								src={
+									'https://i.pinimg.com/736x/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg'
+								}
+								width={'120'}
+								style={{ objectFit: 'cover' }}
+								height={'120'}
+								id='imgProfileUser'
+							/>
+							<span className={'camera text-center py-1'}>
+								<i
+									class='fas fa-camera-retro'
+									style={{ cursor: 'pointer' }}
+									onClick={() => {
+										document.getElementById(
+											'imgupload'
+										).click();
+									}}
+								></i>
+							</span>
+						</div>
+						<input
+							type='file'
+							accept='image/*'
+							style={{ display: 'none' }}
+							id='imgupload'
+							onChange={previewPhoto}
+						/>
+					</div>
+				</Col>
+				<Col lg={6} md={12}>
+					<EditProfileForm />
+				</Col>
+			</Row>
+		</Container>
+	);
 };
